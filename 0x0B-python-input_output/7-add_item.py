dba116034ef2ead8f431bd0313bdf933ca542d06
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-08-03 01:19:30 +0300

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that add all arguments to a Python list
-and then save them to a JSON file
-"""
-import json
-from sys import argv
+""" Module """
+import sys
+from os import path
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    l = load_from_json_file('add_item.json')
-except:
-    l = []
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-for i in range(1, len(argv)):
-    l.append(argv[i])
-
-save_to_json_file(l, 'add_item.json')
+new_list = []
+if path.isfile("add_item.json"):
+    new_list = load_from_json_file("add_item.json")
+save_to_json_file(new_list + sys.argv[1:], "add_item.json")
