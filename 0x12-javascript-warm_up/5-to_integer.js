e3b27ad58046938c3fd54a80eb6fb95216da6328
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-10-05 22:46:04 +0300

    5-to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
+// prints two arguments passed to it, in the following format: "is"
 
-const arg = parseInt(process.argv[2]);
-if (arg) {
-	console.log('My number: ' + arg);
-}
-else{
-	conole.log('Not a number');
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
