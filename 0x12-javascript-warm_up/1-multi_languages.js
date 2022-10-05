161324a7756ca887fb80547c8b1e65d6433827fa
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-10-05 22:42:32 +0300

    multi_language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Pyhton is cool");
-console.log("JavaScript is amazing");
+// It prints 3 lines
+
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
