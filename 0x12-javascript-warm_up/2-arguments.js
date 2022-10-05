c5bc58473d29d2a2611a1f928c1ff088ae3c389a
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-10-05 22:43:25 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+// Prints a message
 
-if (process.argv.length > 3){
-	console.log('Arguments found');
-}
-else if(process.argv.length === 3){
-	console.log('Argument found');}
-else{
-	console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
